763dc16db3710cc359418f915363995d2472021e
spectral27 <gsoldano27@aol.com>
2021-12-05 22:26:14 +0100

    Initial Commit

---
+++ src/main/java/jaxrs_jdbc/entity/Item.java
@@ -0,0 +1,32 @@
+package jaxrs_jdbc.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
+@XmlType(name = "item", propOrder = {
+	    "id",
+	    "value"
+	})
+public class Item {
+
+	private String id;
+	private String value;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
